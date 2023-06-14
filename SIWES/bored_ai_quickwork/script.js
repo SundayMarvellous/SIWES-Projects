0a998293a6828118119289b3ac4ac152b7d5d70d
SundayMarvellous <114695884+SundayMarvellous@users.noreply.github.com>
2023-06-14 16:10:27 +0100

    bored api

--- SIWES/bored_ai_quickwork/script.js
+++ SIWES/bored_ai_quickwork/script.js
@@ -8,16 +8,36 @@
 //     document.querySelector("MyParagraph").innerHTML = activity;
 // }
 // setInterval(getRandomActivity, 3000)
- 
+ //this line of code is for when the activities are showing at intervals
+
+
+// function yourActivity(){
+//     fetch("http://www.boredapi.com/api/activity/")
+//     .then(res => res.json())
+//     .then(data =>{
+//         document.getElementById("activity").innerText = data.activity;
+
+//     })
+// }
+// yourActivity();
+// setInterval(() => {
+//         yourActivity();
+// }, 1000);
+
 function yourActivity(){
+    container.classlist.replace('invisible', 'visible')
+    header.classList.replace('visible', 'invisible')
+    button.style.top ='75%'
+    button.stye.transform ='translate(-50%, -50%) scale(1.4)'
     fetch("http://www.boredapi.com/api/activity/")
     .then(res => res.json())
     .then(data =>{
-        document.getElementById("activity").innerText = data.activity;
-
+       console.log(data)
     })
 }
-yourActivity();
-setInterval(() => {
-        yourActivity();
-}, 1000);
+document.getElementById('add-activity').addEventListener('click', yourActivity)
+
+
+
+
+
