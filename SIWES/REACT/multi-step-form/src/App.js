2f98b87c374d572bf7c909b49cf0e753ba364e09
SundayMarvellous <114695884+SundayMarvellous@users.noreply.github.com>
2023-07-26 13:48:14 +0100

    prac

--- SIWES/REACT/multi-step-form/src/App.js
+++ SIWES/REACT/multi-step-form/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import UserForm from './components/UserForm';
+document.body.classList.add('body');
+
 
 function App() {
   return (
     <div className="App">
-     
+      <UserForm/>
     </div>
   );
 }
---
+++ SIWES/REACT/multi-step-form/src/components/UserForm.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './styles/form_body.css';
+import Header_image from './assets/images/Header_image.svg'
+
+function UserForm() {
+  return (
+
+    <div className='.container'>
+    <img src={Header_image} alt='Header image' />
+
+    </div>
+
+  )
+}
+
+// export default Header_image
+export default UserForm
