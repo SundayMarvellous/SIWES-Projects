918280d560ca0bcc0c4f88a41449688535ff177d
SundayMarvellous <114695884+SundayMarvellous@users.noreply.github.com>
2023-09-01 14:01:49 +0100

    UPDATE

---
+++ SIWES/multi-step-form/script.js
@@ -0,0 +1,11 @@
+
+function isChecked(){
+    var button2 = document.getElementById('button2');
+    var chk = document.getElementById("termsChkbx2");
+    if(chk.checked){
+       chk.button2.style.backgroundColor  = "black";
+    }
+    else{
+       chk.button2.style.backgroundColor  = "red";
+    }
+  }
